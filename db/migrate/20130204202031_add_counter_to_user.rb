e55613d603d7d2997ccbb04a762e64891a57aa85
allomov <lomov.as@gmail.com>
2013-02-05 02:37:29 +0300

    add some git ignores and workers folder

--- db/migrate/20130204202031_add_counter_to_user.rb
+++ db/migrate/20130204202031_add_counter_to_user.rb
@@ -1,6 +1,6 @@
 class AddCounterToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :counter, :integer
+    add_column :users, :counter, :integer, :default => 0
   end
 
   def self.down
